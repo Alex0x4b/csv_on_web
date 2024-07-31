be258dbc0d73c227576b8b012ca6a91dd1722e10
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2024-07-31 21:31:12 +0200

    [api] -add get/get_by_id/add task

--- api/api.py
+++ api/api.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 
 app = FastAPI()
+tasks = []
 
 
-@app.post("/hello")
-async def hello():
-    return "hello"
+class Task(BaseModel):
+    # Ensure input is correct to avoid DB injection or unespected behaviour
+    id: Optional[int] = None  # Handle by data base
+    task: str
+    is_completed: bool = False
+
+
+@app.get("/get_tasks")
+async def get_tasks():
+    return tasks
+
+
+@app.get("/get_tasks/{id}")
+async def get_task_by_id(id: int):
+    task = [task for task in tasks if task.id == id][0]
+    return task
+
+
+@app.post("/add_task")
+async def add_task(task: Task):
+    task.id = len(tasks) + 1
+    tasks.append(task)
+    return task
